824c6d45850fdf77587be6d7cc2b6c13c5a3ef3b
Sohag Hasan <58013666+mdsohaghasan@users.noreply.github.com>
2022-06-04 23:39:00 +0600

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-          primary: "#79DAE8",
-          secondary: "#548CFF",
-          accent: "#ECB365",
+          primary: "#4B11E8",
+          secondary: "#F7D716",
+          accent: "#9BA3EB",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
