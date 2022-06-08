f4d829912cec178a53fa98ec56da053205f501ca
Sohag Hasan <mailsohaghasan@gmail.com>
2022-06-08 23:59:49 +0600

    set img

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,9 @@ module.exports = {
     themes: [
       {
         mytheme: {
-<<<<<<< HEAD
           primary: "#2F8F9D",
           secondary: "#3BACB6",
           accent: "#EEB0B0",
-=======
-          primary: "#4B11E8",
-          secondary: "#F7D716",
-          accent: "#9BA3EB",
->>>>>>> 824c6d45850fdf77587be6d7cc2b6c13c5a3ef3b
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
